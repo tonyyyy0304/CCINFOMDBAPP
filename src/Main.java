058c8004bd34631f5d05060de14702a1722e2ab2
dreiiiiiiiiiii <161920929+dreiiiiiiiiiii@users.noreply.github.com>
2024-11-16 18:27:26 +0800

    update build instructions in Main.java to include additional source files and correct run command

--- src/Main.java
+++ src/Main.java
@@ -19,6 +19,9 @@ public class Main {
 }
 
 /*
- * javac -cp ".;mysql-connector-j-9.1.0.jar" -d bin src/mvc_folder/Model.java src/Main.java
- * java -cp ".;bin;mysql-connector-j-9.1.0.jar" Main
+ * # Compile the Java files
+javac -cp ".;mysql-connector-j-9.1.0.jar" -d bin src/main/java/mvc_folder/Model.java src/main/java/mvc_folder/View.java src/main/java/mvc_folder/Controller.java src/Main.java
+
+# Run the application
+java -cp ".;bin;mysql-connector-j-9.1.0.jar" src.Main
  */
